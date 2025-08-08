90d63d162806d7b99b0ae8d39cf34adc787d8d38
Aleksandr Filippov <swayalex.work@gmail.com>
2025-08-08 17:05:47 +0200

    Fixed types in useVirtuosoLibAsync() hook.

--- helpers/useVirtuosoLibAsync.tsx
+++ helpers/useVirtuosoLibAsync.tsx
@@ -1,4 +1,17 @@
-import {useEffect, useState} from 'react'
+import {ComponentType, Ref, useEffect, useState} from 'react'
+import {
+    GroupedTableVirtuosoHandle as _GroupedTableVirtuosoHandle,
+    GroupedTableVirtuosoProps as _GroupedTableVirtuosoProps,
+    GroupedVirtuosoHandle as _GroupedVirtuosoHandle,
+    GroupedVirtuosoProps as _GroupedVirtuosoProps,
+    TableVirtuosoHandle as _TableVirtuosoHandle,
+    TableVirtuosoProps as _TableVirtuosoProps,
+    VirtuosoGridHandle as _VirtuosoGridHandle,
+    VirtuosoGridProps as _VirtuosoGridProps,
+    VirtuosoHandle as _VirtuosoHandle,
+    VirtuosoProps as _VirtuosoProps,
+} from 'react-virtuoso'
+import {AnyObject} from 'swayok-react-mdb-ui-kit/types/Common'
 
 function Placeholder() {
     return (
@@ -6,21 +19,39 @@ function Placeholder() {
     )
 }
 
-type HookReturn = Pick<
-    typeof import('react-virtuoso/dist/index'),
-    'Virtuoso' | 'VirtuosoGrid' | 'GroupedVirtuoso' | 'TableVirtuoso'
->
+interface HookReturn<ItemData = AnyObject, Context = AnyObject | undefined | null> {
+    Virtuoso: ComponentType<_VirtuosoProps<ItemData, Context> & {
+        ref?: Ref<_VirtuosoHandle>
+    }>
+    TableVirtuoso: ComponentType<_TableVirtuosoProps<ItemData, Context> & {
+        ref?: Ref<_TableVirtuosoHandle>
+    }>
+    GroupedVirtuoso: ComponentType<_GroupedVirtuosoProps<ItemData, Context> & {
+        ref?: Ref<_GroupedVirtuosoHandle>
+    }>
+    GroupedTableVirtuoso: ComponentType<_GroupedTableVirtuosoProps<ItemData, Context> & {
+        ref?: Ref<_GroupedTableVirtuosoHandle>
+    }>
+    // Не включен в react-virtuoso 4.x.x, но используется в 5.x.x и выше.
+    VirtuosoGrid: ComponentType<_VirtuosoGridProps<ItemData, Context> & {
+        ref?: Ref<_VirtuosoGridHandle>
+    }>
+}
 
 // Асинхронная загрузка компонентов react-virtuoso.
-export default function useVirtuosoLibAsync(): HookReturn {
+export default function useVirtuosoLibAsync<
+    ItemData = AnyObject,
+    Context = AnyObject | undefined | null
+>(): HookReturn<ItemData, Context> {
 
     const [
         virtuoso,
         setVirtuoso,
-    ] = useState<HookReturn>(() => ({
+    ] = useState<HookReturn<ItemData, Context>>(() => ({
         Virtuoso: Placeholder,
         GroupedVirtuoso: Placeholder,
         TableVirtuoso: Placeholder,
+        GroupedTableVirtuoso: Placeholder,
         VirtuosoGrid: Placeholder,
     }))
 
@@ -30,6 +61,7 @@ export default function useVirtuosoLibAsync(): HookReturn {
                 Virtuoso: lib.Virtuoso,
                 GroupedVirtuoso: lib.GroupedVirtuoso,
                 TableVirtuoso: lib.TableVirtuoso,
+                GroupedTableVirtuoso: lib.GroupedTableVirtuoso,
                 VirtuosoGrid: lib.VirtuosoGrid,
             }))
             .catch(e => console.log('Failed to load react-virtuoso: ', e))
@@ -37,3 +69,14 @@ export default function useVirtuosoLibAsync(): HookReturn {
 
     return virtuoso
 }
+
+export type GroupedTableVirtuosoHandle = _GroupedTableVirtuosoHandle
+export type GroupedTableVirtuosoProps<ItemData = AnyObject, Context = AnyObject | undefined | null> = _GroupedTableVirtuosoProps<ItemData, Context>
+export type GroupedVirtuosoHandle = _GroupedVirtuosoHandle
+export type GroupedVirtuosoProps<ItemData = AnyObject, Context = AnyObject | undefined | null> = _GroupedVirtuosoProps<ItemData, Context>
+export type TableVirtuosoHandle = _TableVirtuosoHandle
+export type TableVirtuosoProps<ItemData = AnyObject, Context = AnyObject | undefined | null> = _TableVirtuosoProps<ItemData, Context>
+export type VirtuosoGridHandle = _VirtuosoGridHandle
+export type VirtuosoGridProps<ItemData = AnyObject, Context = AnyObject | undefined | null> = _VirtuosoGridProps<ItemData, Context>
+export type VirtuosoHandle = _VirtuosoHandle
+export type VirtuosoProps<ItemData = AnyObject, Context = AnyObject | undefined | null> = _VirtuosoProps<ItemData, Context>
