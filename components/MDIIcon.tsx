8ee5ee959a64d2e2682073ebfc698b9adef51eeb
Aleksandr Filippov <swayalex.work@gmail.com>
2025-09-18 18:50:35 +0200

    Fixed MDIIcon reuse functionality in combination with strict mode.

--- components/MDIIcon.tsx
+++ components/MDIIcon.tsx
@@ -1,39 +1,35 @@
 import * as React from 'react'
-import {AllHTMLAttributes, CSSProperties, useEffect, useId, useRef} from 'react'
-import {AnyObject} from '../types/Common'
+import {AllHTMLAttributes, CSSProperties, SVGAttributes, useId} from 'react'
+import ReusableSvg from 'swayok-react-mdb-ui-kit/components/ReusableSvg'
 
 export interface IconProps extends Omit<AllHTMLAttributes<SVGSVGElement>, 'size' | 'label' | 'width' | 'height'> {
-    id?: string;
-    path: string;
-    ref?: React.RefObject<SVGSVGElement>;
-    title?: string;
-    description?: string | null;
-    horizontal?: boolean;
-    vertical?: boolean;
-    rotate?: number;
-    spin?: boolean | number;
-    style?: CSSProperties;
-    size?: number | null;
+    id?: string
+    path: string
+    ref?: React.RefObject<SVGSVGElement>
+    title?: string
+    description?: string | null
+    horizontal?: boolean
+    vertical?: boolean
+    rotate?: number
+    spin?: boolean | number
+    style?: CSSProperties
+    size?: number | null
     // HTML ID иконки для переиспользования. Допустимые символы: a-z, A-Z, 0-9, -, _.
     // Если на странице большое количество одинаковых иконок, то браузер будет
     // довольно сильно тормозить из-за этого.
     // В этом случае можно использовать возможность копирования содержимого SVG
-    // через тэг <use href="#reusable-svg-icon-{reuse}"/>. Стоит учитывать что
+    // через тэг <use href="#reusable-svg-icon-{reuse}"/>. Стоит учитывать, что
     // в этом случае невозможно будет изменять содержимое SVG, включая цвет
     // через CSS класс поскольку внутри <svg> не будет <path>, <g> и т.п.
-    reuse?: string;
+    reuse?: string
     // CSS класс контейнера, в котором будет сохранён оригинал иконки.
+    // Класс нужно задавать, если стиль иконки зависит от CSS-класса родительского DOM элемента.
     reusableItemContainerClass?: string;
-    // Контейнер для хранения оригиналов иконок, используемых для копирования.
-    reusableItemsContainerSelector?: string | 'body';
 }
 
-// Контейнер с известными иконками доступными для переиспользования.
-const reusableIcons: AnyObject = {}
-
+// Иконка.
 function MDIIcon(props: IconProps) {
     const {
-        id,
         path,
         title,
         description,
@@ -42,158 +38,123 @@ function MDIIcon(props: IconProps) {
         vertical = false,
         rotate = 0,
         spin = false,
-        style: customStyle = {},
+        style: propsStyle = {},
         reuse,
         reusableItemContainerClass,
-        reusableItemsContainerSelector = 'body',
         ...otherProps
     } = props
 
-    const ref = useRef<SVGSVGElement>(null)
-    const reusableIconId: string = 'reusable-svg-icon-' + reuse
-
-    useEffect(() => {
-        if (reuse && ref.current) {
-            // Нам нужно дополнительно сохранить исходный SVG-элемент вне React приложения,
-            // чтобы он хранился там даже при полной перерисовке React приложения.
-            // При первом использовании svg элемент будет вставлен в оригинальном виде.
-            // При повторных перерисовках он будет заменен на <svg><use href="#{reusableIconId}"/></svg>.
-            // Если вне приложения не будет полной копии, то иконка не отобразится.
-            reusableIcons[reuse] = true
-            // Сначала удаляем существующий элемент, если он уже есть.
-            const existing: HTMLElement | null = document.getElementById(reusableIconId)
-            if (existing) {
-                existing.parentElement?.remove()
-            }
-            // Создаем невидимый контейнер для хранения оригинала иконки.
-            const iconContainter: HTMLDivElement = document.createElement('div')
-            iconContainter.style.display = 'none'
-            if (reusableItemContainerClass) {
-                iconContainter.className = reusableItemContainerClass
-            }
-            // Клонируем иконку и добавляем ее в невидимый контейнер.
-            const clone: SVGElement = ref.current.cloneNode(true) as SVGElement
-            clone.id = reusableIconId
-            iconContainter.append(clone)
-
-            // Добавляем невидимый контейнер с иконкой в контейнер где хранятся все подобные элементы
-            if (!reusableItemsContainerSelector || reusableItemsContainerSelector === 'body') {
-                document.body.append(iconContainter)
-            } else {
-                const allIconsContainer = document.body.querySelector(reusableItemsContainerSelector)
-                if (allIconsContainer) {
-                    allIconsContainer.append(iconContainter)
-                } else {
-                    console.error(
-                        '[MDIIcon] failed to find element for reusableItemsContainerSelector = '
-                        + reusableItemsContainerSelector
-                    )
-                    document.body.append(iconContainter)
-                }
-            }
-        }
-    }, [ref.current, reuse])
-
     const index: string = useId()
-    const pathStyle: CSSProperties = {}
-    const transform = []
-    const style = Object.assign({}, customStyle || {})
-    if (size !== null) {
-        style.height = style.width = size + 'px'
-    }
-    if (horizontal) {
-        transform.push('scaleX(-1)')
-    }
-    if (vertical) {
-        transform.push('scaleY(-1)')
-    }
-    if (rotate !== 0) {
-        transform.push(`rotate(${rotate}deg)`)
-    }
-    const transformElement = (
-        <path
-            d={path}
-            style={pathStyle}
-        />
-    )
-    if (transform.length > 0) {
-        style.transform = transform.join(' ')
-        style.transformOrigin = 'center'
-    }
-    let spinElement = transformElement
-    const spinSec = spin || typeof spin !== 'number' ? 2 : spin
-    let inverse = horizontal || vertical
-    if (spinSec < 0) {
-        inverse = !inverse
-    }
-    if (spin) {
-        spinElement = (
-            <g
-                style={{
-                    animation: `spin${inverse ? '-inverse' : ''} linear ${Math.abs(spinSec)}s infinite`,
-                    transformOrigin: 'center',
-                }}
-            >
-                {transformElement}
-                {!(horizontal || vertical || rotate !== 0) && (
-                    <rect
-                        width="24"
-                        height="24"
-                        fill="transparent"
-                    />
-                )}
-            </g>
-        )
-    }
-    let ariaLabelledby
     const labelledById = `icon_labelledby_${index}`
     const describedById = `icon_describedby_${index}`
-    let role
-    if (title) {
-        ariaLabelledby = description
-            ? `${labelledById} ${describedById}`
-            : labelledById
-    } else {
-        role = 'presentation'
-        if (description) {
-            throw new Error('title attribute required when description is set')
+
+    const renderSpinElement = () => {
+        const transformElement = <path d={path} />
+        let spinElement = transformElement
+        const spinSec = spin || typeof spin !== 'number' ? 2 : spin
+        let inverse = horizontal || vertical
+        if (spinSec < 0) {
+            inverse = !inverse
         }
-    }
-    if (reuse && reusableIcons[reuse]) {
-        return (
-            <svg
-                viewBox="0 0 24 24"
-                style={style}
-                role={role}
-                aria-labelledby={ariaLabelledby}
-                data-reuse="1"
-                {...otherProps}
-            >
-                <use href={'#' + reusableIconId}/>
-            </svg>
-        )
-    } else {
-        if (reuse) {
-            reusableIcons[reuse] = true
+        if (spin) {
+            spinElement = (
+                <g
+                    style={{
+                        animation: `spin${inverse ? '-inverse' : ''} linear ${Math.abs(spinSec)}s infinite`,
+                        transformOrigin: 'center',
+                    }}
+                >
+                    {transformElement}
+                    {!(horizontal || vertical || rotate !== 0) && (
+                        <rect
+                            width="24"
+                            height="24"
+                            fill="transparent"
+                        />
+                    )}
+                </g>
+            )
         }
         return (
-            <svg
-                ref={ref}
-                viewBox="0 0 24 24"
-                style={style}
-                role={role}
-                aria-labelledby={ariaLabelledby}
-                id={id}
-                {...otherProps}
-            >
-                {title && <title id={labelledById}>{title}</title>}
-                {description && <desc id={describedById}>{description}</desc>}
+            <>
                 {spin && (
                     inverse
                         ? <style>{'@keyframes spin-inverse { to { transform: rotate(-360deg) } }'}</style>
                         : <style>{'@keyframes spin { to { transform: rotate(360deg) } }'}</style>
                 )}
                 {spinElement}
+            </>
+        )
+    }
+
+    const collectStyles = (): CSSProperties => {
+        const transform = []
+        if (horizontal) {
+            transform.push('scaleX(-1)')
+        }
+        if (vertical) {
+            transform.push('scaleY(-1)')
+        }
+        if (rotate !== 0) {
+            transform.push(`rotate(${rotate}deg)`)
+        }
+        const style: CSSProperties = Object.assign({}, propsStyle || {})
+        if (size !== null) {
+            style.height = style.width = size + 'px'
+        }
+        if (transform.length > 0) {
+            style.transform = transform.join(' ')
+            style.transformOrigin = 'center'
+        }
+        return style
+    }
+
+    const getCommonSvgProps = (): SVGAttributes<SVGElement> => {
+        let ariaLabelledby
+        let role
+        if (title) {
+            ariaLabelledby = description
+                ? `${labelledById} ${describedById}`
+                : labelledById
+        } else {
+            role = 'presentation'
+            if (description) {
+                throw new Error('title attribute required when description is set')
+            }
+        }
+        return {
+            'aria-labelledby': ariaLabelledby,
+            role,
+            viewBox: '0 0 24 24',
+            style: collectStyles(),
+            ...otherProps
+        }
+    }
+
+    const commonProps = getCommonSvgProps()
+    const content = (
+        <>
+            {title && <title id={labelledById}>{title}</title>}
+            {description && <desc id={describedById}>{description}</desc>}
+            {renderSpinElement()}
+        </>
+    )
+
+    if (reuse) {
+        return (
+            <ReusableSvg
+                {...commonProps}
+                reuse={'icon-' + reuse}
+                reusableItemContainerClass={reusableItemContainerClass}
+                uid={index}
+            >
+                {content}
+            </ReusableSvg>
+        )
+    } else {
+        return (
+            <svg {...commonProps}>
+                {content}
             </svg>
         )
     }
---
+++ helpers/ReusableSvgRepository.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react'
+import {ReactNode} from 'react'
+import {AnyObject} from 'swayok-react-mdb-ui-kit/types/Common'
+
+export interface ReusableSvgRepositoryConfig {
+    containerClassName?: string
+}
+
+export type ReusableSvgRepositorySetSvgElementFn = (element: SVGSVGElement | null) => void
+
+type ReusableSvgRepositoryRememberFallbackFn = (resolve: ReusableSvgRepositorySetSvgElementFn) => ReactNode | ReactNode[]
+
+interface CachedSvgElement {
+    uid: string | number
+    children: ReactNode | ReactNode[]
+}
+
+export class ReusableSvgRepository {
+
+    private static container: HTMLDivElement | null = null
+
+    private static svgElements: AnyObject<CachedSvgElement, string> = {}
+
+    private static config: ReusableSvgRepositoryConfig = {}
+
+    static configure(config: ReusableSvgRepositoryConfig) {
+        this.config = {...this.config, ...config}
+        if (this.container) {
+            this.container.className = this.config.containerClassName ?? ''
+        }
+    }
+
+    static getSvgContents(
+        name: string,
+        uid: string | number,
+        fallback: ReusableSvgRepositoryRememberFallbackFn,
+        reusableItemContainerClass?: string,
+    ): ReactNode | ReactNode[] {
+        const reusableId = this.getSvgId(name)
+        if (name in this.svgElements) {
+            return this.svgElements[name].uid === uid
+                ? this.svgElements[name].children
+                : <use href={'#' + reusableId} />
+        }
+        // Запоминаем сразу uid, чтобы в дальнейшем для этого uid отдавать результат.
+        this.svgElements[name] = {
+            uid,
+            children: fallback(
+                element => this.rememberSvgElement(
+                    name,
+                    element,
+                    reusableItemContainerClass
+                )
+            )
+        }
+        return this.svgElements[name].children
+    }
+
+    private static rememberSvgElement(
+        name: string,
+        svgElement: SVGSVGElement | null,
+        reusableItemContainerClass?: string,
+    ): void {
+        if (!svgElement) {
+            return
+        }
+        const reusableId: string = this.getSvgId(name)
+        const existing: HTMLElement | null = document.getElementById(reusableId)
+        if (existing) {
+            existing.parentElement?.remove()
+        }
+        // Создаем контейнер для хранения оригинала иконки.
+        const iconContainer: HTMLDivElement = document.createElement('div')
+        iconContainer.style.display = 'none'
+        if (reusableItemContainerClass) {
+            iconContainer.className = reusableItemContainerClass
+        }
+        // Клонируем иконку и добавляем ее в невидимый контейнер.
+        const clone: SVGSVGElement = svgElement.cloneNode(true) as SVGSVGElement
+        clone.id = reusableId
+        iconContainer.append(clone)
+        this.getContainer().appendChild(iconContainer)
+    }
+
+    private static getContainer(): Node {
+        if (!this.container) {
+            this.container = document.createElement('div')
+            this.container.style.display = 'none'
+            document.body.appendChild(this.container)
+        }
+        return this.container
+    }
+
+    private static getSvgId(name: string): string {
+        return 'reusable-svg-' + name
+    }
+
+
+}
